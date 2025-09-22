88989771d6299de3daa0f177121cb511451abf43
LucasO21 <clfo2014@gmail.com>
2025-09-22 06:16:14 -0400

    rag basics a

--- src/global_utilities/paths.py
+++ src/global_utilities/paths.py
@@ -7,6 +7,8 @@ ROOT_DIR = Path(__file__).resolve().parent.absolute().parent.parent.absolute()
 
 CANNONDALE_BIKES_ASSISTANT_DIR = ROOT_DIR / "src" / "cannondale_bikes_assistant"
 
+LANGCHAIN_BEGINNER_MASTERCLASS_DIR = ROOT_DIR / "src" / "langchain_beginner_masterclass"
+
 # DATABASE_DIR = ROOT_DIR / "database/"
 
 # CRM_SQLITE_DATABASE_DIR = f"sqlite:///{DATABASE_DIR}/00_crm_database.sqlite"
