3eda6c462249719285d90f0f84b449038de11544
LucasO21 <clfo2014@gmail.com>
2025-09-22 04:24:34 -0400

    prompt templates complete

--- src/langchain_beginner_masterclass/02_prompt_templates.py
+++ src/langchain_beginner_masterclass/02_prompt_templates.py
@@ -19,6 +19,22 @@ openai_model = ChatOpenAI(
     api_key=OPENAI_API_KEY
 )
 
+google_model = ChatGoogleGenerativeAI(
+    model='gemini-1.5-flash',
+    temperature=0.1,
+    api_key=GOOGLE_API_KEY
+)
+
+anthropic_model = ChatAnthropic(
+    model='claude-3-5-sonnet-20240620',
+    temperature=0.1,
+    api_key=ANTHROPIC_API_KEY
+)
+
+
+# ------------------------------------------------------------------------------
+# BASIC ----
+# ------------------------------------------------------------------------------
 # # PART 1: Create a ChatPromptTemplate using a template string
 template = "Tell me a joke about {topic}."
 prompt_template = ChatPromptTemplate.from_template(template)
@@ -68,3 +84,40 @@ prompt_template = ChatPromptTemplate.from_messages(messages)
 prompt = prompt_template.invoke({"topic": "lawyers", "joke_count": 3})
 print("\n----- Prompt with System and Human Messages (Tuple) -----\n")
 print(prompt)
+
+
+
+# ------------------------------------------------------------------------------
+# USAGE ----
+# ------------------------------------------------------------------------------
+
+# PART 1: Create a ChatPromptTemplate using a template string
+print("-----Prompt from Template-----")
+template = "Tell me a joke about {topic}."
+prompt_template = ChatPromptTemplate.from_template(template)
+
+prompt = prompt_template.invoke({"topic": "cats"})
+result = anthropic_model.invoke(prompt)
+print(result.content)
+
+# PART 2: Prompt with Multiple Placeholders
+print("\n----- Prompt with Multiple Placeholders -----\n")
+template_multiple = """You are a helpful assistant.
+Human: Tell me a {adjective} short story about a {animal}.
+Assistant:"""
+prompt_multiple = ChatPromptTemplate.from_template(template_multiple)
+prompt = prompt_multiple.invoke({"adjective": "funny", "animal": "panda"})
+
+result = openai_model.invoke(prompt)
+print(result.content)
+
+# PART 3: Prompt with System and Human Messages (Using Tuples)
+print("\n----- Prompt with System and Human Messages (Tuple) -----\n")
+messages = [
+    ("system", "You are a comedian who tells jokes about {topic}."),
+    ("human", "Tell me {joke_count} jokes."),
+]
+prompt_template = ChatPromptTemplate.from_messages(messages)
+prompt = prompt_template.invoke({"topic": "lawyers", "joke_count": 3})
+result = openai_model.invoke(prompt)
+print(result.content)
